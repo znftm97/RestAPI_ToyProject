3ef8498a57303643ad8fb32f26d1f257688e4670
jihoon <znftm97@naver.com>
2021-01-11 19:13:55 +0900

    Swagger 적용

--- src/main/java/com/restful/api/controller/UserController.java
+++ src/main/java/com/restful/api/controller/UserController.java
@@ -2,14 +2,15 @@ package com.restful.api.controller;
 
 import com.restful.api.entity.User;
 import com.restful.api.repository.UserRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. User"})
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/v1")
@@ -17,16 +18,19 @@ public class UserController {
 
     private final UserRepository userRepository;
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원 조회")
     @GetMapping("/user")
     public List<User> findAllUser(){
         return userRepository.findAll();
     }
 
+    @ApiOperation(value = "회원가입")
     @PostMapping("/user")
-    public User save(){
+    public User save(@ApiParam(value = "회원 아이디", required = true) @RequestParam String uid,
+                     @ApiParam(value = "회원 이름", required = true) @RequestParam String name){
         User user = User.builder()
-                .uid("aaa@naver.com")
-                .name("aaa")
+                .uid(uid)
+                .name(name)
                 .build();
         return userRepository.save(user);
     }
